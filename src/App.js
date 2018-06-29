fbea5d2a45a2b4b4da41c54e56baca7aaa4c4c4a
noah398 <869tjmw@gmail.com>
2018-06-29 12:57:03 +0900

    Starting With a Map

--- src/App.js
+++ src/App.js
@@ -1,43 +1,54 @@
 import React, { Component } from 'react';
 import MapGL from 'react-map-gl';
-import DeckGL, {LineLayer, ScatterplotLayer} from 'deck.gl';
 
 // Set your mapbox token here
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN; // eslint-disable-line
+const MAPBOX_STYLE = 'mapbox://styles/mapbox/dark-v9';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             viewport: {
-                latitude: 37.785164,
-                longitude: -122.41669,
-                zoom: 16.14044,
-                bearing: -20.55991,
-                pitch: 60
-            },
-            width: 500,
-            height: 500
+                width: window.innerWidth,
+                height: window.innerHeight,
+                longitude: -74,
+                latitude: 40.7,
+                zoom: 11,
+                maxZoom: 16
+            }
         };
     }
 
+    componentDidMount() {
+        window.addEventListener('resize', this._resize);
+        this._resize();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this._resize);
+    }
+
+    _resize = () => {
+        this._onViewportChange({
+            width: window.innerWidth,
+            height: window.innerHeight
+        });
+    };
+
+    _onViewportChange(viewport) {
+        this.setState({
+            viewport: {...this.state.viewport, ...viewport}
+        });
+    }
 
     render() {
-        const {viewport, width, height} = this.state;
-
         return (
-            <MapGL {...viewport} width={width} height={height} mapboxApiAccessToken={MAPBOX_TOKEN}>
-                <DeckGL {...viewport} width={width} height={height} debug>
-                    <LineLayer
-                        data={[{sourcePosition: [-122.41669, 37.7883], targetPosition: [-122.41669, 37.781]}]}
-                        strokeWidth={5}
-                    />
-                    <ScatterplotLayer
-                        data={[{position: [-122.41669, 37.79]}]}
-                        radiusScale={100}
-                        getColor={x => [0, 0, 255]}
-                    />
-                </DeckGL>
+            <MapGL
+                {...this.state.viewport}
+                onViewportChange={viewport => this._onViewportChange(viewport)}
+                mapStyle={MAPBOX_STYLE}
+                mapboxApiAccessToken={MAPBOX_TOKEN}>
             </MapGL>
         );
     }
